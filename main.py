6a4a2845540dedc1decbb4c4b994c7bb67e273ef
Henry Carpenter <hcarpenter@cnsstu.catholic.edu.au>
2021-03-10 16:48:07 +1000

    added data folder

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import pygame
 
-
 pygame.init()
-pygame.font.init() #initilising pygame and pygame font
+pygame.font.init() 
 font = pygame.font.Font("data/font.ttf", 50)
-smallFont = pygame.font.Font("data/font.ttf", 10) #initilising fonts from the ./data directory
+smallFont = pygame.font.Font("data/font.ttf", 10) 
 window = pygame.display.set_mode((500, 300))
 
-class button(): #button class
+class button():
     def __init__(self, x, y, width, height, image, value):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.image = pygame.image.load(image)
-        self.value = value #initilises variables of class from input
+        self.value = value
     def render(self):
-        window.blit(self.image, (self.x, self.y)) #custom render function
+        window.blit(self.image, (self.x, self.y))
     def collision(self, mousePosX, mousePosY):
-        if (mousePosX > self.x and mousePosX < self.x + self.width and 
-                mousePosY > self.y and mousePosY < self.y + self.height):
-                #spliting if statment is used for clarity
+        if (mousePosX > self.x and mousePosX < self.x + self.width and mousePosY > self.y and mousePosY < self.y + self.height):
             return True
-        return False #checks if colliding
+        return False
+
+menu = [ button(7, 7, 7, 7, 7, 0),
+         button(7, 7, 7, 7, 7, 0)]
