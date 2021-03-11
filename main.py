9c232f5d8e43c2f9aafb82d3cd3c173db7205cf1
Henry Carpenter <hcarpenter@cnsstu.catholic.edu.au>
2021-03-11 13:02:23 +1000

    Added functioning buttons

--- main.py
+++ main.py
@@ -2,26 +2,37 @@ import pygame
 
 pygame.init()
 pygame.font.init() 
-font = pygame.font.Font("data/font.ttf", 50)
-smallFont = pygame.font.Font("data/font.ttf", 10) 
-window = pygame.display.set_mode((500, 300))
+font = pygame.font.Font("data/font.ttf", 40)
+window = pygame.display.set_mode((600, 300))
+pygame.display.set_caption("Tuckshop")
 
 class button():
-    def __init__(self, x, y, width, height, image, value):
+    def __init__(self, x, y, width, height, cost, item):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.image = pygame.image.load(image)
-        self.value = value
+        self.cost = font.render(str(cost), False, (255, 255, 255))
+        self.price = cost
+        self.item = font.render(item, False, (255, 255, 255))
     def render(self):
-        window.blit(self.image, (self.x, self.y))
+        pygame.draw.rect(window, (0,0,0) , (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(window, (255,255,255), (self.x, self.y, self.width, self.height), 3)
+        window.blit(self.item, (self.x + 10, self.y + 10))
+        window.blit(self.cost, (self.x +  self.width - (10 + (40 * len(str(self.price)))), self.y + 10))
     def collision(self, mousePosX, mousePosY):
         if (mousePosX > self.x and mousePosX < self.x + self.width and mousePosY > self.y and mousePosY < self.y + self.height):
             return True
         return False
 
-menu = [ button(7, 7, 7, 7, 7, 0),
+menu = [ button(50, 10, 500, 50, 7, "chocolate milk"),
         ]
 
+def draw():
+    window.fill((0,0,0))
+    for item in menu:
+        item.render()
+    pygame.display.flip()
 
+while 1:
+    draw()
