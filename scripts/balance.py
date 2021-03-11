47051c8f806414c659b57e18be5f215a27e81951
Henry Carpenter <hcarpenter@cnsstu.catholic.edu.au>
2021-03-11 10:31:38 +1000

    file read

--- scripts/balance.py
+++ scripts/balance.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-def test():
-    Balance = open('data')
-=======
 def READ():
     dataFile = open("data/balance", 'r')
     balance = float(dataFile.read())
@@ -10,4 +6,3 @@ def READ():
 
 def DUMP(balance):
     return balance
->>>>>>> b9dd1e0c4bf8db3e445741e1fd34784fc60067a4
